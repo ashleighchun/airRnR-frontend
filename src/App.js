9e643ef9eee6b6959d71af74dd85c9da74a54067
Ashleigh Chun <ashleigh.chun@gmail.com>
2019-09-12 20:54:48 -0700

    add component mount

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component {
+
+  componentDidMount(){
+    fetch('http://localhost:3000/api/v1/trips/1', { //returns a promise that we will eventually get data back
+      method: 'GET'
+    })
+    .then(response => response.json()) //then turn the fetch response into a json file
+    .then(data => console.log(data[0]))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        Hello world
+      </div>
+    );
+  }
 }
 
 export default App;
